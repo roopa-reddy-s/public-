12097ff2ec830caa1543fe4c30d135a085ec7964
roopareddy <105273810+roopa-reddy-s@users.noreply.github.com>
2022-09-05 19:37:52 +0530

    Add files via upload

--- TestRiunner_ex.java
+++ TestRiunner_ex.java
@@ -17,13 +17,11 @@ public class TestRiunner_ex extends Test{
 	public static Loginpage lp = new Loginpage(driver);
 	public static void main(String[] args) throws IOException {
 		
-		getUrl("http://automationpractice.com/index.php");
-		clickOnElement(hp.getSigIn());		
-		inputvalueElement(lp.getEmail(),"roopa@gmail.com");			
-		inputvalueElement(lp.getPassword(),"roopa1234");		
-		clickOnElement(lp.getSignin_Btn());	
-		screenshot("Loginpage");		
-		driver.close();
+		
+//		inputvalueElement(lp.getPassword(),"roopa1234");		
+//		clickOnElement(lp.getSignin_Btn());	
+//		screenshot("Loginpage");		
+//		driver.close();
 		
 	
 		
